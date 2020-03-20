0abf271df91efc4c538e621802d1d4ba90856875
Sliwer <stefanalexjieanu@gmail.com>
2020-03-20 15:09:22 +0200

    Python to show generated image

---
+++ gen_image.py
@@ -0,0 +1,25 @@
+import numpy
+import cv2
+
+with open('image_data.txt', 'r') as f:
+    d = f.readline().split(' ')
+    image_height = int(d[0])
+    image_width = int(d[1])
+
+    image_data = numpy.empty([image_height, image_width])
+
+    # The image data as an array
+    d = f.readline().split(' ')
+
+    # Counts each element of the image data
+    # so that we can convert it into a matrix with for loops
+    counter = 0 
+
+    for i in range(0, image_height):
+        for j in range(0, image_width):
+            image_data[i][j] = counter
+            counter += 1
+
+cv2.imshow('image', image_data)
+cv2.waitKey(0)
+quit()
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int res = system("python gen.py");
+    int res = system("python gen_image.py");
     if (res != 0)
         printf("Exit code was: %d", res);
 
